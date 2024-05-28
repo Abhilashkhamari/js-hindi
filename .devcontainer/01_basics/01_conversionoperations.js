5b2ef8fb2557a446d417fe7b3ef33635efe06079
Abhilashkhamari <143178173+Abhilashkhamari@users.noreply.github.com>
2024-05-28 05:34:26 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/01_conversionoperations.js
@@ -0,0 +1,24 @@
+let score="Abhilash"
+
+//console.log(typeof score);
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1 ; false=>0
+
+let isLoggedIn="Ahilash"
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//true=>1 ; false=>0
+// "" =>false
+// "Abhilash" => true
+
+let somenumber=33
+let stringnumber=String(somenumber)
+console.log(stringnumber)
+console.log(typeof stringnumber)
