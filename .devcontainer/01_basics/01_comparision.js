82ff64179e2ccf8890320e04ff7b2ff212639a30
Abhilashkhamari <143178173+Abhilashkhamari@users.noreply.github.com>
2024-05-28 06:40:13 +0000

    Conversion

---
+++ .devcontainer/01_basics/01_comparision.js
@@ -0,0 +1,17 @@
+// so many comparision operator used in it i.e <,>,<=,>=,==
+// console.log(null > 0)
+// console.log(null >= 0)
+// console.log(null <= 0)
+// console.log(null == 0)
+
+// console.log(undefined > 0)
+// console.log(undefined >= 0)
+// console.log(undefined<= 0)
+// console.log(undefined== 0)
+//  so we should avoid this type of comparision
+// console.log("2">1)
+console.log("2"==2)
+
+// === it is strict check.It check the datatype also, if the datatype is not same it gives false value.
+
+console.log("2" === 2)
--- .devcontainer/01_basics/01_conversionoperations.js
+++ .devcontainer/01_basics/01_conversionoperations.js
@@ -1,24 +1,46 @@
-let score="Abhilash"
+// let score="Abhilash"
 
-//console.log(typeof score);
-// let valueInNumber=Number(score)
-// console.log(typeof valueInNumber)
-// console.log(valueInNumber)
+// //console.log(typeof score);
+// // let valueInNumber=Number(score)
+// // console.log(typeof valueInNumber)
+// // console.log(valueInNumber)
 
-//"33"=>33
-//"33abc"=>NaN
-//true=>1 ; false=>0
+// //"33"=>33
+// //"33abc"=>NaN
+// //true=>1 ; false=>0
 
-let isLoggedIn="Abhilash"
+// let isLoggedIn="Abhilash"
 
-let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
-//true=>1 ; false=>0
-// "" =>false
-// "Abhilash" => true
+// //true=>1 ; false=>0
+// // "" =>false
+// // "Abhilash" => true
 
-let somenumber=33
-let stringnumber=String(somenumber)
-console.log(stringnumber)
-console.log(typeof stringnumber)
+// let somenumber=33
+// let stringnumber=String(somenumber)
+// console.log(stringnumber)
+// console.log(typeof stringnumber)
+
+// let value=3
+// let negetivevalue=-value
+// console.log(negetivevalue)
+
+// str1="ABhilash"
+// str2=" pratik"
+// str3=str1+str2
+// console.log(str3)
+
+console.log(1+3+"2")
+console.log("3"+4+5)
+
+console.log(+true)
+console.log(true)
+console.log(+"")
+console.log(+false)
+console.log(false)
+
+let gameCounter=100
+gameCounter++ //in prefic value is change before operation but in postfix value are change after operation
+console.log(gameCounter)
